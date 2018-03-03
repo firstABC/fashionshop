73bc0889ed1718b88510f58e2c6ec16876f90c49
窦仁钰 <@LAPTOP-4DP9TMIF>
2018-03-03 17:02:05 +0800

    add mylike

---
+++ src/main/java/chen/mingyu/domain/MyLike.java
@@ -0,0 +1,29 @@
+package chen.mingyu.domain;
+
+public class MyLike {
+	
+	private String ml_id;
+	private String g_id;
+	//用户id
+	private String userId;
+	public String getMl_id() {
+		return ml_id;
+	}
+	public void setMl_id(String ml_id) {
+		this.ml_id = ml_id;
+	}
+	public String getG_id() {
+		return g_id;
+	}
+	public void setG_id(String g_id) {
+		this.g_id = g_id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
